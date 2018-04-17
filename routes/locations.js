edc17553236f2031523d981870c17e46faf4216f
Sergi Domenech Guzy <sergiguzy@web.de>
2018-04-18 01:14:59 +0200

    server testing

--- routes/locations.js
+++ routes/locations.js
@@ -68,6 +68,8 @@ router.get('/page', token.checkToken(), (req, res) => {
 router.get('/byid/:_id', token.checkToken(), (req, res) => {
 	Location.findOne({ _id: req.params._id })
 		.then(location => {
+			console.log(location);
+			
 			if (!location) {
 				return res.status(400).json({ message: 'No location found with this ID', token: res.locals.token });
 			}
