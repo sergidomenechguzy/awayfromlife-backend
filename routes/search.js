4f00c9feae12a1b804a6485970e07e202adf8f0b
Sergi Domenech Guzy <sergiguzy@web.de>
2018-05-06 22:11:30 +0200

    updated search route to check more attributes

--- routes/search.js
+++ routes/search.js
@@ -16,6 +16,8 @@ const Band = mongoose.model('bands');
 
 // load token.js
 const token = require('../config/token');
+// load dereference.js
+const dereference = require('../config/dereference');
 
 // search routes
 // get all elements
@@ -53,42 +55,69 @@ router.get('/', token.checkToken(), (req, res) => {
 
 // get all search results
 router.get('/:query', token.checkToken(), (req, res) => {
-	const regex = '.*' + req.params.query + '.*';
+	const regex = RegExp('.*' + req.params.query + '.*', 'gi');
 	let responseList = [];
 
-	Event.find({ title: new RegExp(regex, 'gi') })
+	Event.find()
 		.then(events => {
-			responseList = events.map(event => {
-				return { category: 'Event', title: event.title, id: event._id };
-			});
-
-			Location.find({ name: new RegExp(regex, 'gi') })
-				.then(locations => {
-					responseList = responseList.concat(locations.map(location => {
-						return { category: 'Location', title: location.name, id: location._id };
-					}));
-
-					Band.find({ name: new RegExp(regex, 'gi') })
-						.then(bands => {
-							responseList = responseList.concat(bands.map(band => {
-								return { category: 'Band', title: band.name, id: band._id };
-							}));
-
-							responseList.sort((a, b) => {
-								return a.title.localeCompare(b.title);
-							});
-
-							return res.status(200).json({ data: responseList, token: res.locals.token });
-						})
-						.catch(err => {
-							console.log(err.name + ': ' + err.message);
-							return res.status(500).json({ message: 'Error, something went wrong. Please try again.' });
-						});
-				})
-				.catch(err => {
+			dereference.eventObjectArray(events, 'title', 1, (err, responseEvents) => {
+				if (err) {
 					console.log(err.name + ': ' + err.message);
 					return res.status(500).json({ message: 'Error, something went wrong. Please try again.' });
+				}
+				responseEvents.forEach(event => {
+					if (regex.test(event.title) 
+						|| regex.test(event.location.name) 
+						|| regex.test(event.location.address.street) 
+						|| regex.test(event.location.address.city) 
+						|| regex.test(event.location.address.country)) 
+							responseList.push({category: 'Event', data: event});
+					else {
+						event.bands.some(band => {
+							if (regex.test(band.name)) {
+								responseList.push({category: 'Event', data: event});
+								return true;
+							}
+						});
+					}
 				});
+
+				Location.find()
+					.then(locations => {
+						locations.forEach(location => {
+							if (regex.test(location.name) 
+								|| regex.test(location.address.street) 
+								|| regex.test(location.address.city) 
+								|| regex.test(location.address.country)) 
+									responseList.push({category: 'Location', data: location});
+						});
+
+						Band.find()
+							.then(bands => {
+								bands.forEach(band => {
+									if (regex.test(band.name) 
+										|| regex.test(band.genre) 
+										|| regex.test(band.origin.name) 
+										|| regex.test(band.origin.country)) 
+											responseList.push({category: 'Band', data: band});
+								});
+
+								// responseList.sort((a, b) => {
+								// 	return a.title.localeCompare(b.title);
+								// });
+	
+								return res.status(200).json({ data: responseList, token: res.locals.token });
+							})
+							.catch(err => {
+								console.log(err.name + ': ' + err.message);
+								return res.status(500).json({ message: 'Error, something went wrong. Please try again.' });
+							});
+					})
+					.catch(err => {
+						console.log(err.name + ': ' + err.message);
+						return res.status(500).json({ message: 'Error, something went wrong. Please try again.' });
+					});
+			});
 		})
 		.catch(err => {
 			console.log(err.name + ': ' + err.message);
