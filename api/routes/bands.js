65c712fde60eb721720271be700dde3106da559b
sergidomenechguzy <sergiguzy@web.de>
2019-05-26 17:55:21 +0200

    hardcoded tmp band genre update

--- api/routes/bands.js
+++ api/routes/bands.js
@@ -451,11 +451,735 @@ router.post('/convertCSV', token.checkToken(false), multerConfig.uploadCSV.singl
 	}
 });
 
-const convertJson = require(dirPath + '/api/helpers/convertJson');
-router.put('/update/Tmp/Genre', multerConfig.uploadJSON.single('file'), async (req, res) => {
+router.get('/update/Tmp/Genre', async (req, res) => {
 	try {
 		console.log('start');
-		const jsonFile = await convertJson.convertFile(req.file.path);
+		const jsonFile = [
+			{
+			  "_id": "5adc666a2beb9202f15dde55",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad994322beb9202f15ddd6f",
+			  "genre": "Post Punk"
+			},
+			{
+			  "_id": "5ae1826528c2a6710464c57d",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad994352beb9202f15ddd71",
+			  "genre": "Indie Punk"
+			},
+			{
+			  "_id": "5ad994362beb9202f15ddd72",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5ae1826528c2a6710464c57e",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5ad994382beb9202f15ddd73",
+			  "genre": "Post Hardcore"
+			},
+			{
+			  "_id": "5adc66412beb9202f15dde53",
+			  "genre": "Melodic Punk"
+			},
+			{
+			  "_id": "5adc667e2beb9202f15dde56",
+			  "genre": "Melodic Punk"
+			},
+			{
+			  "_id": "5adc669d2beb9202f15dde58",
+			  "genre": "Skate Punk"
+			},
+			{
+			  "_id": "5adcc5b72beb9202f15dde73",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8ece02beb9202f15ddcb7",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8f27f2beb9202f15ddccb",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5adf446228c2a6710464c52d",
+			  "genre": "Horror Punk"
+			},
+			{
+			  "_id": "5adcc5c12beb9202f15dde75",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ae972f528c2a6710464c5fa",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5adf44cb28c2a6710464c531",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8cdf7d4491577b8e6b9cc",
+			  "genre": "Pop Punk"
+			},
+			{
+			  "_id": "5af5f84428c2a6710464c68e",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad994392beb9202f15ddd74",
+			  "genre": "Straight Edge"
+			},
+			{
+			  "_id": "5ad8ec772beb9202f15ddcb3",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5adf8a8828c2a6710464c542",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5adf8a8a28c2a6710464c543",
+			  "genre": "Horror Punk"
+			},
+			{
+			  "_id": "5b44c17ff1d2541b9e1a59f9",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5ae1826628c2a6710464c57f",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ae1826828c2a6710464c580",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad994552beb9202f15ddd77",
+			  "genre": "Post Hardcore"
+			},
+			{
+			  "_id": "5ae1826828c2a6710464c581",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad994442beb9202f15ddd76",
+			  "genre": "Post Hardcore"
+			},
+			{
+			  "_id": "5ae1826b28c2a6710464c585",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ae1826b28c2a6710464c586",
+			  "genre": "Reggae Punk"
+			},
+			{
+			  "_id": "5adf451428c2a6710464c537",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ada44a72beb9202f15dde01",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad994582beb9202f15ddd79",
+			  "genre": "Straight Edge"
+			},
+			{
+			  "_id": "5addb7c52beb9202f15ddebd",
+			  "genre": "UK82"
+			},
+			{
+			  "_id": "5adf44f328c2a6710464c535",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ae2b63528c2a6710464c5a3",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5afb2c2128c2a6710464c6ce",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5addb6922beb9202f15ddeaf",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ae1826d28c2a6710464c588",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5ae2e4f328c2a6710464c5c3",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5af5f4ae28c2a6710464c662",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5af5f57028c2a6710464c66c",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5af5f63c28c2a6710464c676",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5af5f61c28c2a6710464c674",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5ae4d9d028c2a6710464c5e8",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ada3f8d2beb9202f15dddcf",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5adf44b628c2a6710464c52f",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5af5f6af28c2a6710464c67a",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5af5f6e928c2a6710464c67e",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5af0a29928c2a6710464c61c",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5af139a128c2a6710464c62b",
+			  "genre": "Streetpunk"
+			},
+			{
+			  "_id": "5adefb2c28c2a6710464c514",
+			  "genre": "Psychobilly"
+			},
+			{
+			  "_id": "5ad9950e2beb9202f15ddd7c",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5af2f1eb28c2a6710464c63e",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5ae9735f28c2a6710464c5fe",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5addb7e52beb9202f15ddebf",
+			  "genre": "UK82"
+			},
+			{
+			  "_id": "5ad8fdc32beb9202f15ddd03",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5ad9950f2beb9202f15ddd7d",
+			  "genre": "Pop Punk"
+			},
+			{
+			  "_id": "5b44c180f1d2541b9e1a5a00",
+			  "genre": "Beatdown"
+			},
+			{
+			  "_id": "5ae9738c28c2a6710464c600",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5ad8eb162beb9202f15ddca7",
+			  "genre": "Folk Punk"
+			},
+			{
+			  "_id": "5af2f16528c2a6710464c635",
+			  "genre": "Hip Hop"
+			},
+			{
+			  "_id": "5ad8f22e2beb9202f15ddcc7",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5adefb2c28c2a6710464c517",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5b44c181f1d2541b9e1a5a03",
+			  "genre": "Folk Punk"
+			},
+			{
+			  "_id": "5ad8eb0f2beb9202f15ddca5",
+			  "genre": "Folk Punk"
+			},
+			{
+			  "_id": "5ada41862beb9202f15ddddf",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5af5f7fa28c2a6710464c68a",
+			  "genre": "Punk"
+			},
+			{
+			  "_id": "5af5f82228c2a6710464c68c",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5addb8042beb9202f15ddec1",
+			  "genre": "UK82"
+			},
+			{
+			  "_id": "5ad995102beb9202f15ddd80",
+			  "genre": "Melodic Punk"
+			},
+			{
+			  "_id": "5ad8ecb12beb9202f15ddcb5",
+			  "genre": "Pop Punk"
+			},
+			{
+			  "_id": "5ada452f2beb9202f15dde07",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ada44442beb9202f15dddfd",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5afb2c2428c2a6710464c6d2",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad8f4882beb9202f15ddcdb",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad995112beb9202f15ddd82",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5af5dfc828c2a6710464c65b",
+			  "genre": "Pop Punk"
+			},
+			{
+			  "_id": "5ad995122beb9202f15ddd83",
+			  "genre": "Pop Punk"
+			},
+			{
+			  "_id": "5afb2c2528c2a6710464c6d3",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5adcc5d12beb9202f15dde78",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5afb2c2628c2a6710464c6d4",
+			  "genre": "Post Hardcore"
+			},
+			{
+			  "_id": "5b44c182f1d2541b9e1a5a09",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad9b2372beb9202f15dddb5",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5afb2c2928c2a6710464c6d8",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5ae1827028c2a6710464c58e",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5adcc5af2beb9202f15dde72",
+			  "genre": "Post Punk"
+			},
+			{
+			  "_id": "5afb2c2a28c2a6710464c6d9",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ada42b52beb9202f15ddded",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad8fdc52beb9202f15ddd04",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5af1377228c2a6710464c624",
+			  "genre": "Streetpunk"
+			},
+			{
+			  "_id": "5ad995132beb9202f15ddd85",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5ad8e5f72beb9202f15ddc87",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ada3fe22beb9202f15dddd3",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad995142beb9202f15ddd86",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5addb6952beb9202f15ddeb4",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5ad8fdca2beb9202f15ddd06",
+			  "genre": "Ska Punk"
+			},
+			{
+			  "_id": "5ad995142beb9202f15ddd87",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5addb6962beb9202f15ddeb5",
+			  "genre": "Streetpunk"
+			},
+			{
+			  "_id": "5addb6962beb9202f15ddeb6",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ada3fc12beb9202f15dddd1",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5af5f87b28c2a6710464c690",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad995152beb9202f15ddd88",
+			  "genre": "Skate Punk"
+			},
+			{
+			  "_id": "5ad8f37f2beb9202f15ddcd3",
+			  "genre": "Skate Punk"
+			},
+			{
+			  "_id": "5ad995152beb9202f15ddd89",
+			  "genre": "Skate Punk"
+			},
+			{
+			  "_id": "5ad8eb182beb9202f15ddca8",
+			  "genre": "Skate Punk"
+			},
+			{
+			  "_id": "5ae1827228c2a6710464c592",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8eb192beb9202f15ddca9",
+			  "genre": "Ska Punk"
+			},
+			{
+			  "_id": "5ad995162beb9202f15ddd8a",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5ad995172beb9202f15ddd8b",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ae4d99e28c2a6710464c5e6",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad995182beb9202f15ddd8c",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad9951d2beb9202f15ddd8e",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5ad9951c2beb9202f15ddd8d",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ae2e4f828c2a6710464c5cb",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad8eb1a2beb9202f15ddcaa",
+			  "genre": "Streetpunk"
+			},
+			{
+			  "_id": "5ad995262beb9202f15ddd91",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8e5e92beb9202f15ddc85",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ae2e4f828c2a6710464c5cc",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5addbaee2beb9202f15ddecf",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5ad995272beb9202f15ddd92",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5b44c185f1d2541b9e1a5a15",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad995272beb9202f15ddd93",
+			  "genre": "Pop Punk"
+			},
+			{
+			  "_id": "5b44c185f1d2541b9e1a5a16",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5b44c186f1d2541b9e1a5a17",
+			  "genre": "Streetpunk"
+			},
+			{
+			  "_id": "5af5f66a28c2a6710464c678",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5ada44842beb9202f15dddff",
+			  "genre": "Straight Edge"
+			},
+			{
+			  "_id": "5ad995282beb9202f15ddd94",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad8fdcc2beb9202f15ddd07",
+			  "genre": "Melodic Punk"
+			},
+			{
+			  "_id": "5adcc5a72beb9202f15dde71",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ada47a52beb9202f15dde1b",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad8e5402beb9202f15ddc7d",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad8fdce2beb9202f15ddd08",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad8f2b22beb9202f15ddccd",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad9952b2beb9202f15ddd98",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad8eb1d2beb9202f15ddcac",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad9952b2beb9202f15ddd99",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ada42f72beb9202f15dddf1",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ada43cc2beb9202f15dddf9",
+			  "genre": "Horror Punk"
+			},
+			{
+			  "_id": "5ad9952c2beb9202f15ddd9a",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5b44c186f1d2541b9e1a5a1a",
+			  "genre": "Streetpunk"
+			},
+			{
+			  "_id": "5addb6982beb9202f15ddeb9",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5ae1827428c2a6710464c595",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad9b2382beb9202f15dddb6",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5ad8f3d52beb9202f15ddcd7",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5b44c187f1d2541b9e1a5a1c",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5ad9b2382beb9202f15dddb7",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5ae2e4fa28c2a6710464c5cf",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad9b2392beb9202f15dddb8",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad9b23a2beb9202f15dddba",
+			  "genre": "Crossover"
+			},
+			{
+			  "_id": "5ad8f2d62beb9202f15ddccf",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5adf44dd28c2a6710464c533",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad9b23b2beb9202f15dddbb",
+			  "genre": "Punk'n'Roll"
+			},
+			{
+			  "_id": "5ad9b23b2beb9202f15dddbc",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8fdd02beb9202f15ddd09",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad8eb1e2beb9202f15ddcae",
+			  "genre": "UK82"
+			},
+			{
+			  "_id": "5ada46402beb9202f15dde0f",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ad9b23b2beb9202f15dddbd",
+			  "genre": "Melodic Hardcore"
+			},
+			{
+			  "_id": "5ad9b23c2beb9202f15dddbe",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ada415e2beb9202f15ddddd",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8f44d2beb9202f15ddcd9",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad9b23c2beb9202f15dddbf",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8e1c42beb9202f15ddc7a",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5b44c189f1d2541b9e1a5a1f",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8ec092beb9202f15ddcaf",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ada3f4b2beb9202f15dddcd",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ae6e30928c2a6710464c5ea",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5af5dfd528c2a6710464c65f",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad8efe82beb9202f15ddcbf",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ad9b23c2beb9202f15dddc1",
+			  "genre": "Punk Rock"
+			},
+			{
+			  "_id": "5ada41112beb9202f15ddddb",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ada41b42beb9202f15ddde1",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ae4d99f28c2a6710464c5e7",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5b44c189f1d2541b9e1a5a20",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5ada425e2beb9202f15ddde9",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ae1827528c2a6710464c598",
+			  "genre": "Metal Hardcore"
+			},
+			{
+			  "_id": "5ada42302beb9202f15ddde7",
+			  "genre": "Hardcore"
+			},
+			{
+			  "_id": "5ad8ee312beb9202f15ddcb9",
+			  "genre": "Hardcore Punk"
+			},
+			{
+			  "_id": "5ae973bb28c2a6710464c602",
+			  "genre": "Deutschpunk"
+			},
+			{
+			  "_id": "5ada3f4c2beb9202f15dddce",
+			  "genre": "Straight Edge"
+			}
+		  ];
 		console.log('after json', jsonFile);
 		const promises = jsonFile.map(async jsonObject => {
 			try {
